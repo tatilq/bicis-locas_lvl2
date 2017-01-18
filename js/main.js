dcda49dda5d70222a2d66535cc7188fe803856e6
Tatiana Limache Quispe <tati34che@gmail.com>
2017-01-18 08:48:22 -0500

    Cambios nuevos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-    var textNombre="Nombre validos";
+//span.style.display="none";
+//funcion que cree nodos 
 function crearNodo(	contenedor , mensaje)
 {
 	var nodo = document.createElement("span");
 	nodo.innerText = mensaje;
 	contenedor.appendChild(nodo);
 }
-
 //validar que todos los campos esten llenos
 function validateForm()
 {
@@ -14,16 +14,20 @@ function validateForm()
   var lastname = document.getElementById("lastname").value;    
   if( nombre.length == 0 && lastname.length == 0 && validaEmail()==false && validaPass()==false && validaBici()==false)
   { 
-    salida_name.innerHTML="<span style='color:red; font-size:15px;' >Debe Ingresar su Nombre</span>";  
-    salida_lastname.innerHTML="<span style='color:red; font-size:15px;' >Debe  Ingresar su Apellido</span>"; 
+  	salida_name.innerHTML="<span style='color:white; font-size:15px;' >Debe Ingresar su Nombre</span>";  
+    salida_lastname.innerHTML="<span style='color:white; font-size:15px;' >Debe  Ingresar su Apellido</span>"; 
     salida_mail.innerHTML="<span style='color:red; font-size:15px;' >Debe Ingresar su correo</span>";
     salida_pass.innerHTML="<span style='color:red; font-size:15px;'>Debe Ingresar su Contraseña</span";
     salida_bici.innerHTML="<span style='color:red; font-size:15px;'>Debe seleccionar una bici</span>";
-    
+    salida_form.innerHTML='<p style="color:red;">Faltan llenar Campos</p> ✔';
+   	return false;
+  } 
+  if( nombre.length != 0 && lastname.length != 0 && validaEmail()==true && validaPass()==true && validaBici()==true)
+  { 
+	salida_form.innerHTML='<p style="color:green;">Formulario enviado</p> ✔';
    	return false;
   } 
 }	
-/*
 //valida solo letras en el campo de nombres y apellidos
 function checkInput(evt)
 {
@@ -33,15 +37,16 @@ function checkInput(evt)
   else
   	//salida_name.innerHTML="<span style='color:red; font-size:15px;' >Debes escribir letras</span>";
     evt.preventDefault();
-} */
+} 
 //convierte la primera letra en mayuscula
 function validaName() 
 {
-    var lastname = document.getElementById("name").value;    
-    var nombreArray = lastname.split("");
+    var name = document.getElementById("name").value;    
+    var nombreArray = name.split("");
     var primeraLetra = nombreArray[0];
     var primeraMayus = primeraLetra.toUpperCase();
     var cortePalabra = false;
+
      for(var i=1;i<nombreArray.length;i++)
         {
           if(cortePalabra)
@@ -54,6 +59,7 @@ function validaName()
           if(nombreArray[i] == " ")
               cortePalabra = true;
         } 
+    var textNombre="Nombre Valido ✔";
 	crearNodo(contenedorName,textNombre);
     document.getElementById("name").value = primeraMayus; 
  }
@@ -77,9 +83,9 @@ function validaLastname()
           if(nombreArray[i] == " ")
               cortePalabra = true;
         } 
-
-        salida_lastname.innerHTML="<span style='color:white; font-size:15px;' >Apellido valido ?</span>";  
-        document.getElementById("lastname").value = primeraMayus; 
+	var textLastname="Apellido Valido ✔";
+    crearNodo(contenedorLastname,textLastname);
+    document.getElementById("lastname").value = primeraMayus; 
 }
 //valida el email segun el formato valido
 function validaEmail() 
@@ -87,15 +93,16 @@ function validaEmail()
   
   var email = document.getElementById("input-email").value;
   var salida_mail = document.getElementById("salida_mail");
-  
+  var textEmailFalla="Formato Invalido, Ej: name@domain.com";
+  var textEmailValido="Email valido ✔";
   if (!email.match(/^[A-Za-z\._\-0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) 
   {
-    salida_mail.innerHTML="<span style='color:red; font-size:15px;' >Formato Invalido, Ej: name@domain.com</span>";
+  	crearNodo(contenedorEmail,textEmailFalla);
     return false;
   }
   else 
   {
-    salida_mail.innerHTML="<span style='color:white; font-size:15px;'>Email valido ?</span>";
+    crearNodo(contenedorEmail,textEmailValido);
     return true;
   }
   
@@ -106,35 +113,40 @@ function validaPass()
 {
   var password = document.getElementById("input-password").value;
   var salida_pass = document.getElementById("salida_pass");
+  var textPassFalla="Minimo 6 caracteres";
+  var textPasValida="Contraseña valida ✔";
+  var textPasInvalida="Contraseña Invalida";
   if (!password.match(/.{6,}/)) 
   {
-    salida_pass.innerHTML="<span style='color:red; font-size:15px;'>Minimo 6 caracteres</span>";
+  	crearNodo(contenedorPassword,textPassFalla);
     return false;
   }
   else if ( password == "123456" || password.toLowerCase() == "password" || password == "098754") 
   {
-    salida_pass.innerHTML="<span style='color:red; font-size:15px;' >Contraseña Invalida</span>";
+    crearNodo(contenedorPassword,textPasInvalida);
     return false;
   }
   else 
   {
-    salida_pass.innerHTML="<span style='color:white; font-size:15px;'>Contraseña valida ?</span";
+    crearNodo(contenedorPassword,textPasValida);
     return true;
   }
 }
 function validaBici()
-
 {
   var tipoBici = document.getElementById("select_bici").value;
   var salida_bici = document.getElementById("salida_bici");
+  var textBici="Bici Seleccionada  ✔";
+  var textBiciInv="Selecciona una Bici";
   if (tipoBici != 0)
   {
-     salida_bici.innerHTML="<span style='color:white; font-size:15px;'>Bici Seleccionada ?</span>";
+  	
+  	crearNodo(contenedorBici,textBici);
     return true; 
   }
   else 
   {
-    salida_bici.innerHTML="<span style='color:red; font-size:15px;'>Selecciona una Bici</span>";
+  	crearNodo(contenedorBici,textBiciInv);
     return false;
     
   }
